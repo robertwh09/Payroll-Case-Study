c1dab2c20b6aad6163056ca5e682b4af2e0b28d7
robertwh09 <47435375+robertwh09@users.noreply.github.com>
2021-02-14 23:01:54 +0000

    First MySQL test case passes

---
+++ Payroll/SqlPayrollDatabase.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Payroll.Database
+{
+   public class SqlPayrollDatabase : IPayrollDatabase
+   {
+      private readonly MySql.Data.MySqlClient.MySqlConnection conn;
+      public SqlPayrollDatabase()
+      {
+         string connString = "server=localhost;user id=sa;database=payroll;pwd=abc";
+         conn = new MySql.Data.MySqlClient.MySqlConnection(connString);
+         conn.Open();
+      }
+      public void AddEmployee(Employee employee)
+      {
+         string sql = "insert into employee values (" +
+         "@EmpId, @Name, @Address, @ScheduleType, " +
+         "@PaymentMethodType, @PaymentClassificationType)";
+
+         MySqlCommand command = new MySqlCommand();
+         command.Connection = conn;
+         command.CommandText = sql;
+
+         command.Parameters.AddWithValue("@EmpId", employee.EmpId);
+         command.Parameters.AddWithValue("@Name", employee.Name);
+         command.Parameters.AddWithValue("@Address", employee.Address);
+         command.Parameters.AddWithValue("@ScheduleType", ScheduleCode(employee.Schedule));
+         command.Parameters.AddWithValue("@PaymentMethodType", employee.Method.GetType().ToString());
+         command.Parameters.AddWithValue("@PaymentClassificationType", employee.Classification.GetType().ToString());
+         command.ExecuteNonQuery();
+      }
+      private static string ScheduleCode(PaymentSchedule schedule)
+      {
+         if (schedule is MonthlySchedule)
+            return "monthly";
+         else
+            return "unknown";
+      }
+
+      public void AddUnionMember(int id, Employee e)
+      {
+         throw new System.NotImplementedException();
+      }
+
+      public void DeleteEmployee(int id)
+      {
+         throw new System.NotImplementedException();
+      }
+
+      public ArrayList GetAllEmployeeIds()
+      {
+         throw new System.NotImplementedException();
+      }
+
+      public IList GetAllEmployees()
+      {
+         throw new System.NotImplementedException();
+      }
+
+      public Employee GetEmployee(int id)
+      {
+         throw new System.NotImplementedException();
+      }
+
+      public Employee GetUnionMember(int id)
+      {
+         throw new System.NotImplementedException();
+      }
+
+      public void RemoveUnionMember(int memberId)
+      {
+         throw new System.NotImplementedException();
+      }
+   }
+}
---
+++ PayrollTests/SqlPayrollDatabaseTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Payroll.Database;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Payroll.Tests
+{
+   [TestClass()]
+   public class SqlPayrollDatabaseTest
+   {
+      private IPayrollDatabase database;
+      private MySqlConnection conn;
+
+      [TestInitialize()]
+      public void SetUp()
+      {
+         this.database = new SqlPayrollDatabase();
+         string connString = "Database=Payroll;Data Source=localhost;user id=sa;password=abc";
+         conn = new MySql.Data.MySqlClient.MySqlConnection(connString); 
+         conn.Open();
+         MySqlCommand cmd = new MySqlCommand("delete from Employee", conn);
+         cmd.ExecuteNonQuery();
+         conn.Close();
+      }
+
+      [TestCleanup()]
+      public void TearDown()
+      {
+         conn.Close();
+      }
+
+      [TestMethod()]
+      public void AddEmployeeTest()
+      {
+         Employee employee = new Employee(123, "George", "123 Baker St.");
+         employee.Schedule = new MonthlySchedule();
+         employee.Method = new DirectDepositMethod("Bank 1", "123890");
+         employee.Classification = new SalariedClassification(1000.00);
+         database.AddEmployee(employee);
+
+         MySqlDataAdapter adpt = new MySqlDataAdapter("select * from Employee", conn);
+         DataSet dataset = new DataSet();
+         adpt.Fill(dataset);
+         DataTable table = dataset.Tables["table"];
+         Assert.AreEqual(1, table.Rows.Count);
+         DataRow row = table.Rows[0];
+         Assert.AreEqual(123, row["EmpId"]); Assert.AreEqual("George", row["Name"]);
+         Assert.AreEqual("123 Baker St.", row["Address"]);
+         Assert.AreEqual("monthly", row["ScheduleType"]);
+      }
+   }
+}
